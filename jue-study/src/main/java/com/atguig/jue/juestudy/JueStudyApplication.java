bae596787a58fd328f57cf90da6d1a46f3729081
陈万八 <1216299599@qq.com>
2019-02-19 10:05:12 +0800

    abc

---
+++ jue-study/src/main/java/com/atguig/jue/juestudy/JueStudyApplication.java
@@ -0,0 +1,13 @@
+package com.atguig.jue.juestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JueStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JueStudyApplication.class, args);
+	}
+
+}
